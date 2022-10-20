692287a4718227e9f9a1cb8e8db57aae65ebff59
Ashan Tharindu <ashantharindu@192.168.1.3>
2022-10-20 20:38:58 +0530

    feat: employee update done

--- src/routes/employeeRoutes.js
+++ src/routes/employeeRoutes.js
@@ -5,6 +5,9 @@ import { authenticate } from '../middlewares/authHandler';
 const router = Router();
 router.get('/', authenticate, employeeController.getEmployees);
 router.post('/', employeeController.registerEmployee);
+router.patch('/:id', employeeController.updateEmployee);
+router.delete('/:id', employeeController.registerEmployee);
+router.post('/csv-import', employeeController.registerEmployee);
 
 
 export default router;
